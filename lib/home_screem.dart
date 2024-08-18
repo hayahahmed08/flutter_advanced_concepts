ee2fad043b551a6e1104608665a517afe4a2c12f
hayahahmed08 <hayahahmeduser2@gmail.com>
2024-08-18 23:52:42 +0500

    fixed import

--- lib/home_screem.dart
+++ lib/home_screem.dart
@@ -1,1 +1,19 @@
 import 'package:flutter/material.dart';
+//allows to create widgets
+
+
+//creating a widget HomeScreen
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_advanced_concepts/home_screem.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: ;
+      home: HomeScreen(), // Changed semicolon to comma
     );
   }
 }
