663a2cfd51b17c1c71f6302a5dfa1233a5f01dfd
hayahahmed08 <hayahahmeduser2@gmail.com>
2024-08-18 23:43:20 +0500

    package import

--- lib/main.dart
+++ lib/main.dart
@@ -4,3 +4,17 @@ void main() {
   runApp(const MyApp());
 }
 
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: ;
+    );
+  }
+}
