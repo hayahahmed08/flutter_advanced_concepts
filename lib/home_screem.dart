b250aae2239fc027598d1be2dd884faf942fd946
hayahahmed08 <hayahahmeduser2@gmail.com>
2024-08-21 09:48:37 +0500

    Update home_screem.dart

--- lib/home_screem.dart
+++ lib/home_screem.dart
@@ -29,14 +29,38 @@ class _HomeScreenState extends State<HomeScreen> {
       drawer: Drawer(
         child: SafeArea(
           child: ListView(
+            padding: EdgeInsets.zero,
             children: [
-              DrawerHeader(
-                child: Text('Options'),
-              ),
+              //DrawerHeader(
+                //child: Text('Options'),
+              //),
               UserAccountsDrawerHeader(
+                decoration: BoxDecoration(
+                  color: Colors.black,
+
+                ),
+                currentAccountPicture: CircleAvatar(
+                  backgroundImage: NetworkImage('https://cdn-icons-png.flaticon.com/512/25/25231.png'),
+                ),
                 accountName: Text('hayahahmed'),
                 accountEmail: Text('hayahahmeduser@gmail.com'),
               ),
+              ListTile(
+                leading: Icon(Icons.home),
+                title: Text('Page 1'),
+                onTap: (){
+                  Navigator.pushNamed(context, ScreenTwo.id);
+
+                },
+              ),
+              ListTile(
+                leading: Icon(Icons.calculate),
+                title: Text('Page 2'),
+                onTap: (){
+                  Navigator.pushNamed(context, HomeScreen.id);
+
+                },
+              )
             ],
           ),
         ),
